d8ce9d36b2380eb2cd7db7e083d87aa50420c716
venkateshblks <venkateshblks@gmail.com>
2023-09-28 23:36:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# deepai-chatbot
+ it is a web-based tool designed to harness the capabilities of the DeepAI Text Generator API. This project empowers users to interact with the DeepAI API, allowing them to input text prompts and receive AI-generated textual content as output.
---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+ 
+let inp=document.querySelector('#inp')
+let inj =document.getElementById("inp")
+
+inp.addEventListener("input",autor,false)
+function autor(){
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+    if(this.scrollHeight>150){
+
+        this.style.overflow = 'scroll';
+    }
+
+}
+let int=document.getElementById("int")
+let ou=document.getElementById("out")
+let b=document.getElementById("btn")
+const chata=document.querySelector(".chat")
+
+b.addEventListener("click",function(){
+    show()
+})
+
+inp.addEventListener("keyup",function(e){
+    if(e.key==="Enter"){
+
+    show()
+
+    }
+
+})
+function show(){
+    const inval=inp.value;
+    
+    if (inval.trim() === "") return;
+    console.log(inval)
+    appendMessage("int",inval+"🧑")
+    runinp(inval)
+    // appendMessage("out","🤖"+inval);
+   
+    inp.value='';
+   
+}
+function appendMessage(type, message) {
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("message", type);
+    messageDiv.textContent = message;
+    chata.appendChild(messageDiv);
+    chata.scrollTop=chata.scrollHeight;
+ 
+}
+// });
+
+
+function runinp(textgen)
+{
+    const url = 'https://api.deepai.org/api/text-generator'; // API endpoint
+const apiKey = 'Your DeepAI API key'; // Replace with your actual API key
+
+const formData = new FormData();
+formData.append('text', textgen);
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'api-key': apiKey,
+  },
+  body: formData,
+})
+  .then(response => {
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok (${response.status})`);
+    }
+   
+    return response.json();
+  })
+  .then(data => {
+   
+    appendMessage("out","🤖"+data.output);
+    console.log(data.output);
+  })
+
+}
+ 
+ 
